bb51bc14df806b46931ff064a6c82638174e3de6
AmBplus <96792239+AmBplus@users.noreply.github.com>
2022-05-29 17:03:15 +0430

    Add project files.

---
+++ Loging/Controllers/LoginController.cs
@@ -0,0 +1,26 @@
+﻿using Loging.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Loging.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(IFormCollection form)
+        {
+            if (!ModelState.IsValid) return View();
+            return RedirectToAction("Succeed");
+        }
+
+        public IActionResult Succeed(User user)
+        {
+            return View();
+        }
+    }
+}
---
+++ Loging/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Loging.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
